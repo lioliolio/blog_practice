34d3e1e331d17398caffdbe6fd94fdd685291cee
lioliolio <kpc00100@gmail.com>
2016-11-29 17:16:09 +0900

    refactor controller name to "views" and update room view: crawl room data from zigbang api

--- blog/blog/controller.py
+++
@@ -1,7 +0,0 @@
-from django.http.response import HttpResponse
-
-def home(request):
-    return HttpResponse("Hello World!")
-
-def room(request, room_id):
-    return HttpResponse("This is a room detail " + room_id)
