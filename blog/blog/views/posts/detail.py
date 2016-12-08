5feece80c0b379f4b1aa871eb43b659513bf74b6
lioliolio <kpc00100@gmail.com>
2016-12-08 15:16:12 +0900

    update posts detail view

--- blog/blog/views/posts/detail.py
+++ blog/blog/views/posts/detail.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
 
 def detail(request, post_id):
     return render(
         request,
         "posts/detail.html",
-        {},
+        {
+            "post": Post.objects.get(id=post_id),
+        }
     )
