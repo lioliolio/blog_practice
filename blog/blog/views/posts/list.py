510c9952336d608702ba94af5aa8b3f29b0c69fd
lioliolio <kpc00100@gmail.com>
2016-12-08 15:08:24 +0900

    update posts list view

--- blog/blog/views/posts/list.py
+++ blog/blog/views/posts/list.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
 
 def list(request):
     return render(
         request,
         "posts/list.html",
-        {},
+        {
+            "posts": Post.objects.all(),
+        },
     )
