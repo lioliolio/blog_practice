837e6e7dbf901cf9b868b87c7caa979712f21cca
lioliolio <kpc00100@gmail.com>
2016-12-12 16:48:38 +0900

    update naver_post list view: add query search feature

--- blog/blog/views/naver_posts.py
+++ blog/blog/views/naver_posts.py
@@ -5,15 +5,20 @@ from blog.models import NaverPost
 
 def naver_posts_list(request):
     keyword = request.GET.get("keyword")
+    query = request.GET.get("query")
     naver_posts = NaverPost.objects.all()
 
     if keyword:
         naver_posts = naver_posts.filter(keyword=keyword)
 
+    if query:
+        naver_posts = naver_posts.filter(title__contains=query)
+
     return render(
         request,
         "naver_posts/list.html",
         {
+            "query": query,
             "keyword": keyword,
             "naver_posts": naver_posts,
         },
