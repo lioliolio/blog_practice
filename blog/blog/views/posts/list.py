2eecfff9ebf60d2d771fc559f17890754b5be1dc
lioliolio <kpc00100@gmail.com>
2016-12-21 16:56:25 +0900

    update PostListView: add pagination feature

--- blog/blog/views/posts/list.py
+++ blog/blog/views/posts/list.py
@@ -8,3 +8,6 @@ from .base import PostBaseView
 class PostListView(PostBaseView, ListView):
     template_name = "posts/list.html"
     context_object_name = "posts"
+
+    def get_paginate_by(self, query_set):
+        return self.request.GET.get("per", 5)
