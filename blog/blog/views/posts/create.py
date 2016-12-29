3caa14ed23c228151f7a2fd5b18c43880e0cd3b6
lioliolio <kpc00100@gmail.com>
2016-12-29 23:18:54 +0900

    update PostCreateView: via CreateView

--- blog/blog/views/posts/create.py
+++ blog/blog/views/posts/create.py
@@ -1,21 +1,15 @@
-from django.views.generic import View
-from django.shortcuts import redirect
+from django.views.generic.edit import CreateView
 
-from blog.models import Post
+from .base import PostBaseView
 
 
-class PostCreateView(View):
+class PostCreateView(PostBaseView, CreateView):
+    fields = [
+        "title",
+        "content",
+        "image",
+    ]
 
-    def post(self, request, *args, **kwargs):
-        title = request.POST.get("title")
-        content = request.POST.get("content")
-        image = request.FILES.get("image")
-
-        post = Post.objects.create(
-            user=request.user,
-            title=title,
-            content=content,
-            image=image,
-        )
-
-        return redirect(post)
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(PostCreateView, self).form_valid(form)
