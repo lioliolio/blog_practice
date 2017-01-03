6b14918a00902363d6bf8b5faed4a1383999cfc4
lioliolio <kpc00100@gmail.com>
2017-01-03 21:03:46 +0900

    init BitlinkModelTestCase: test bitlink should have shorten_hash

---
+++ blog/bitly/test.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from bitly.models.bitlink import Bitlink
+
+
+class BitlinkModelTestCase(TestCase):
+
+    def test_bitlink_should_have_shorten_hash(self):
+        # create test user
+        test_username = "test_username"
+        test_password = "test_password"
+        test_user = User.objects.create_user(
+            username=test_username,
+            password=test_password,
+        )
+
+        # create test bitlink
+        test_original_url = "http://www.example.com"
+        bitlink = Bitlink.objects.create(
+            user=test_user,
+            original_url=test_original_url,
+        )
+
+        self.assertTrue(
+            bitlink.shorten_hash,
+        )
